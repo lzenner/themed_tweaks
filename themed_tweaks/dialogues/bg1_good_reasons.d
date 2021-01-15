d5b9b4aaa2d6e2c7139ea94e618ec0b16ec0ff5e
Lauriel <lzenner@live.com>
2021-01-14 20:01:04 -1000

    If RE1 is installed, have Joia give the book normally found next door

--- themed_tweaks/dialogues/bg1_good_reasons.d
+++ themed_tweaks/dialogues/bg1_good_reasons.d
@@ -6,3 +6,15 @@ ALTER_TRANS JOIA
 	BEGIN 0 END
 	BEGIN 0 1 2 END
 	BEGIN "ACTION" ~SetGlobal("#L_GRJoia","GLOBAL",1)~ END
+
+EXTEND_BOTTOM JOIA 7
+	IF ~GlobalGT("TICamryn","GLOBAL",0)~ THEN GOTO JOIA_GIVE_BOOK
+END
+
+APPEND JOIA
+	IF ~~ THEN BEGIN JOIA_GIVE_BOOK
+		SAY @2004 // ~I have this old book you can have.  It's all I have to give you in thanks.~
+		IF ~~ THEN DO ~AddexperienceParty(400) ReputationInc(1) TakePartyItem("RINGJOIA") GiveItemCreate("TITOME01",Player1,0,0,0) EraseJournalEntry(227188) EscapeArea()~ SOLVED_JOURNAL #227189 EXIT
+	END
+END
+
