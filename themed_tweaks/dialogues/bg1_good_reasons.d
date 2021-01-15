0fbe00cd97f18e0c49445012a13709b956378043
Lauriel <lzenner@live.com>
2021-01-14 22:00:13 -1000

    Finished Good Reasons in FAI

--- themed_tweaks/dialogues/bg1_good_reasons.d
+++ themed_tweaks/dialogues/bg1_good_reasons.d
@@ -18,3 +18,19 @@ APPEND JOIA
 	END
 END
 
+APPEND BENTLY
+	IF WEIGHT #-99 ~GlobalGT("TICamryn","GLOBAL",0) Global("#L_GRBently","MYAREA",0)~ THEN BEGIN BENTLY_GIVE_BOOK
+		SAY @2005 // ~It's a right shame you got yourself attacked right outside my door.  I never!  Well here...it's not much, but I hope it helps smooth out any ruffles.~
+		IF ~~ THEN DO ~GiveItemCreate("TITOME02",Player1,0,0,0)~ GOTO BENTLY_GO_UPSTAIRS		
+	END
+	
+	IF WEIGHT #-98 ~Global("#L_GRBently","MYAREA",0)~ THEN BEGIN BENTLY_GO_UPSTAIRS
+		SAY @2006 // ~You look like sturdy types that could help folks out.  There's a couple folk upstairs that need some work done, if you're interested.~
+		IF ~~ THEN DO ~SetGlobal("#L_GRBently","MYAREA",1) StartDialogueNoSet(LastTalkedToBy(Myself))~ EXIT
+	END
+	
+	IF WEIGHT #-97 ~PartyHasItem("RINGJOIA") Global("#L_GRBentlyJoia","MYAREA",0)~ THEN BEGIN JOIAS_RING
+		SAY @2007 // ~That looks like Joia's ring.  I know she'd really appreciate see it again.  She lives in the first house you come to, on the left, when you come in through the gates.~
+		IF ~~ THEN DO ~SetGlobal("#L_GRBentlyJoia","MYAREA",1) StartDialogueNoSet(LastTalkedToBy(Myself))~ EXIT
+	END
+END
