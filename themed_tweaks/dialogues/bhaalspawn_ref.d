93f59d6b71c310f0afb1f100d1fe068948176180
Lauriel <lzenner@live.com>
2021-04-02 15:57:45 -1000

    Amelia and GorLet dialogue changes set into variables
    Also made sure X#AjTamokoKnowBhaal is set in Tamoko's dialogue whenever locals set to the group for them knowing about PC's heritage.  It's used in response triggers.

--- themed_tweaks/dialogues/bhaalspawn_ref.d
+++ themed_tweaks/dialogues/bhaalspawn_ref.d
@@ -13,21 +13,20 @@ REPLACE_TRIGGER_TEXT %SHARTEEL_BANTER% ~Global("P#KnowBhaal","GLOBAL"~ ~Global("
 REPLACE_TRIGGER_TEXT %SHARTEEL_JOINED% ~Global("P#KnowBhaal","GLOBAL"~ ~Global("#L_BSRefKnows","LOCALS"~
 REPLACE_TRIGGER_TEXT TETHTO2 ~Global("P#KnowBhaal","GLOBAL",1)~ ~TriggerOverride(Player1,Global("#L_BSRefKnows","LOCALS",1))~
 REPLACE_TRIGGER_TEXT TETHTO2 ~Global("P#KnowBhaal","GLOBAL",0)~ ~TriggerOverride(Player1,Global("#L_BSRefKnows","LOCALS",0))~
-REPLACE_TRIGGER_TEXT X#AMELIA ~Global("P#KnowBhaal","GLOBAL",1)~ ~TriggerOverride(Player1,Global("#L_BSRefKnows","LOCALS",1))~
 REPLACE_TRIGGER_TEXT %XAN_JOINED% ~Global("P#KnowBhaal","GLOBAL"~ ~Global("#L_BSRefKnows","LOCALS"~
 REPLACE_TRIGGER_TEXT %XZAR_BANTER% ~Global("P#KnowBhaal","GLOBAL"~ ~Global("#L_BSRefKnows","LOCALS"~
 
 %DIALOG_DYN_BXAN%
 %DIALOG_DYN_BXZAR%
-
-REPLACE_ACTION_TEXT X#GORLET ~SetGlobal("P#KnowBhaal","GLOBAL",1)~ ~ActionOverride(Player1,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player2,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player3,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player4,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player5,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player6,SetGlobal("#L_BSRefKnows","LOCALS",1))~
-
-REPLACE_ACTION_TEXT TAMOKO ~SetGlobal("X#HalfBrotherRevealed","GLOBAL",1)~ ~ActionOverride(Player1,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player2,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player3,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player4,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player5,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player6,SetGlobal("#L_BSRefSarvSib","LOCALS",1))~
-
-REPLACE_ACTION_TEXT TAMOKO ~SetGlobal("P#KnowBhaal","GLOBAL",1)~ ~ActionOverride(Player1,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player2,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player3,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player4,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player5,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player6,SetGlobal("#L_BSRefKnows","LOCALS",1))~
+%DIAL_AMELIA%
+%DIAL_GORLET%
 
 REPLACE_ACTION_TEXT TAMOKO ~SetGlobal("X#AjTamokoKnowBhaal","GLOBAL",1)~ ~~
 REPLACE_ACTION_TEXT TAMOKO ~SetGlobal("X#TamokoBhaal","GLOBAL",1)~ ~~
+
+REPLACE_ACTION_TEXT TAMOKO ~SetGlobal("X#HalfBrotherRevealed","GLOBAL",1)~ ~ActionOverride(Player1,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player2,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player3,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player4,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player5,SetGlobal("#L_BSRefSarvSib","LOCALS",1)) ActionOverride(Player6,SetGlobal("#L_BSRefSarvSib","LOCALS",1))~
+
+REPLACE_ACTION_TEXT TAMOKO ~SetGlobal("P#KnowBhaal","GLOBAL",1)~ ~ActionOverride(Player1,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player2,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player3,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player4,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player5,SetGlobal("#L_BSRefKnows","LOCALS",1)) ActionOverride(Player6,SetGlobal("#L_BSRefKnows","LOCALS",1)) SetGlobal("X#AjTamokoKnowBhaal","GLOBAL",1)~
 
 ADD_TRANS_ACTION TAMOKO
 	BEGIN 12 END
